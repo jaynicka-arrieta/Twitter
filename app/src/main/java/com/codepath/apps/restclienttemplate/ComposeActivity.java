438d8eeffd936095885b8b7319969d25943343b2
Jaynicka Arrieta <kiira@fb.com>
2018-07-05 15:17:30 -0700

    color of character count changes if character count drops below 0

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -1,19 +1,36 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class ComposeActivity extends AppCompatActivity {
 
+    TextView tvChar;
     EditText etCompose;
     Button btSubmit;
 
-    private TwitterClient client;
+    TwitterClient client;
+    ArrayList<Tweet> tweets;
+    TweetAdapter tweetAdapter;
+
 
 
     @Override
@@ -21,22 +38,72 @@ public class ComposeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
 
+        //init the arraylist (data source)
+        tweets = new ArrayList<>();
+        //construct adapter from data source
+        tweetAdapter = new TweetAdapter(tweets);
+
         client = TwitterApplication.getRestClient(this);
         etCompose = (EditText) findViewById(R.id.etCompose);
         btSubmit = (Button) findViewById(R.id.btSubmit);
+        tvChar = (TextView) findViewById(R.id.tvChar);
 
+        editTextCounter();
+        sendToTimeline();
+
+    }
+
+    private void sendToTimeline() {
         btSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String message = etCompose.getText().toString();
-                JsonHttpResponseHandler handler = new JsonHttpResponseHandler();
-                client.sendTweet(message, handler);
-                
+                client.sendTweet(message, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        Tweet tweet = new Tweet();
+                        tweets.add(tweet);
+                        tweetAdapter.notifyItemInserted(tweets.size()-1);
+
+                        Intent i = new Intent(getApplicationContext(), TimelineActivity.class);
+                        Toast toast = Toast.makeText(getApplicationContext(), "Successfully tweeted", Toast.LENGTH_SHORT);
+                        View view = toast.getView();
+                        TextView text = (TextView) view.findViewById(android.R.id.message);
+                        text.setTextColor(Color.DKGRAY);
+                        toast.show();
+                        startActivity(i);
+                    }
+                });
+
             }
         });
     }
 
+    private void editTextCounter() {
+        etCompose.addTextChangedListener(new TextWatcher() {
 
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tvChar.setText(String.valueOf(140-etCompose.length()));
+                if (tvChar.getText().toString().equals("-1")) {
+                    tvChar.setTextColor(Color.RED);
+                }
+                if (tvChar.getText().toString().equals("0")) {
+                    tvChar.setTextColor(Color.GRAY);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
 
 
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -21,10 +21,12 @@ import com.loopj.android.http.RequestParams;
  * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
  * 
  */
+
+
 public class TwitterClient extends OAuthBaseClient {
 	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance(); // Change this
 	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = "zFXFYzsX2FZ2O4SCNxSWe9qPZ";       // Change this
+	public static final String REST_CONSUMER_KEY = "zFXFYzsX2FZ2O4SCNxSWe9qPZ";    // Change this
 	public static final String REST_CONSUMER_SECRET = "8qHoBL4wgqFvbj3H2ExLGfcIXLSN6pDFMvZplmUi8mDefVFIpk"; // Change this
 
 	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
